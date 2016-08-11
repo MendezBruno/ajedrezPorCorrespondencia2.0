c17e7fb03ade663f23b3f639c1e9f5fdc913bd61
MendezBruno <brunomendez88@hotmail.com>
2016-08-11 13:00:42 -0300

    primera aplicacion completa

--- AjedrezPorCorrespondencia/app/src/main/java/com/example/bruno/ajedrezporcorrespondencia/MainActivity.java
+++ AjedrezPorCorrespondencia/app/src/main/java/com/example/bruno/ajedrezporcorrespondencia/MainActivity.java
@@ -3,18 +3,34 @@ package com.example.bruno.ajedrezporcorrespondencia;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
+
+   private EditText et1,et2;
+    private TextView tv3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et1=(EditText)findViewById(R.id.et1);
+        et2=(EditText)findViewById(R.id.et2);
+        tv3=(TextView)findViewById(R.id.tv3);
     }
 
-    @Override
-    public void onClick(View v) {
 
+    //Este método se ejecutará cuando se presione el botón
+    public void sumar(View view) {
+        String valor1=et1.getText().toString();
+        String valor2=et2.getText().toString();
+        int nro1=Integer.parseInt(valor1);
+        int nro2=Integer.parseInt(valor2);
+        int suma=nro1+nro2;
+        String resu=String.valueOf(suma);
+        tv3.setText(resu);
     }
 }
