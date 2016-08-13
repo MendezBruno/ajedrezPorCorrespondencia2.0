81e44047bff2ae1b808f9708f76e809bc30e143d
mariano <marianoguido@yahoo.com.ar>
2016-08-13 03:32:06 -0300

    AGregado tablero con colores utilizando la classe Image Adapter

---
+++ app/src/main/java/com/example/bruno/ajedrezporcorrespondencia/ImageAdapter.java
@@ -0,0 +1,81 @@
+package com.example.bruno.ajedrezporcorrespondencia;
+
+import android.content.Context;
+import android.widget.BaseAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by maria on 13/8/2016.
+ */
+public class ImageAdapter extends BaseAdapter {
+    // Contexto de la aplicación
+    private Context mContext;
+
+
+//    // Array de identificadores
+      private Integer[] mThumbIds =
+    {
+    R.drawable.casillero1,R.drawable.casillero2, R.drawable.casillero1, R.drawable.casillero2, R.drawable.casillero1,R.drawable.casillero2,R.drawable.casillero1,R.drawable.casillero2,
+    R.drawable.casillero2,R.drawable.casillero1, R.drawable.casillero2, R.drawable.casillero1, R.drawable.casillero2,R.drawable.casillero1,R.drawable.casillero2,R.drawable.casillero1,
+    R.drawable.casillero1,R.drawable.casillero2, R.drawable.casillero1, R.drawable.casillero2, R.drawable.casillero1,R.drawable.casillero2,R.drawable.casillero1,R.drawable.casillero2,
+    R.drawable.casillero2,R.drawable.casillero1, R.drawable.casillero2, R.drawable.casillero1, R.drawable.casillero2,R.drawable.casillero1,R.drawable.casillero2,R.drawable.casillero1,
+    R.drawable.casillero1,R.drawable.casillero2, R.drawable.casillero1, R.drawable.casillero2, R.drawable.casillero1,R.drawable.casillero2,R.drawable.casillero1,R.drawable.casillero2,
+    R.drawable.casillero2,R.drawable.casillero1, R.drawable.casillero2, R.drawable.casillero1, R.drawable.casillero2,R.drawable.casillero1,R.drawable.casillero2,R.drawable.casillero1,
+    R.drawable.casillero1,R.drawable.casillero2, R.drawable.casillero1, R.drawable.casillero2, R.drawable.casillero1,R.drawable.casillero2,R.drawable.casillero1,R.drawable.casillero2,
+    R.drawable.casillero2,R.drawable.casillero1, R.drawable.casillero2, R.drawable.casillero1, R.drawable.casillero2,R.drawable.casillero1,R.drawable.casillero2,R.drawable.casillero1
+
+    };
+
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    public int getThumbId(int position){
+        return mThumbIds[position];
+
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //ImageView a retornar
+        ImageView imageView;
+
+        if (convertView == null) {
+            /*
+            Crear un nuevo Image View de 90x90
+            y con recorte alrededor del centro
+             */
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(90,90));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(20,20,20,20);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        //Setear la imagen desde el recurso drawable
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+}
+
--- app/src/main/java/com/example/bruno/ajedrezporcorrespondencia/MainActivity.java
+++ app/src/main/java/com/example/bruno/ajedrezporcorrespondencia/MainActivity.java
@@ -3,34 +3,38 @@ package com.example.bruno.ajedrezporcorrespondencia;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.GridView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
-   private EditText et1,et2;
+    private EditText et1, et2;
     private TextView tv3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //new ImageAdapter(this);
+        ImageAdapter ia;
+//        Toast toast = Toast.makeText(this, "llego hasta aca", Toast.LENGTH_SHORT);
+//        toast.show();
+        GridView gridview = (GridView) findViewById(R.id.tablero);
+        ia = new ImageAdapter(this);
+        gridview.setAdapter(ia);
+        Toast toast = Toast.makeText(this,ia.getThumbId(0),Toast.LENGTH_SHORT);
+        toast.show();
 
-        et1=(EditText)findViewById(R.id.et1);
-        et2=(EditText)findViewById(R.id.et2);
-        tv3=(TextView)findViewById(R.id.tv3);
-    }
-
-
-    //Este método se ejecutará cuando se presione el botón
-    public void sumar(View view) {
-        String valor1=et1.getText().toString();
-        String valor2=et2.getText().toString();
-        int nro1=Integer.parseInt(valor1);
-        int nro2=Integer.parseInt(valor2);
-        int suma=nro1+nro2;
-        String resu=String.valueOf(suma);
-        tv3.setText(resu);
     }
 }
+
+
+
+
+
+
+
