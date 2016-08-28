466409b2ac66fb1d851ee37bc452ed91b04ece25
mariano <marianoguido@yahoo.com.ar>
2016-08-27 22:31:57 -0300

    Integro twitter a la aplicacion

--- app/src/main/java/com/example/bruno/ajedrezporcorrespondencia/MainActivity.java
+++ app/src/main/java/com/example/bruno/ajedrezporcorrespondencia/MainActivity.java
@@ -8,16 +8,27 @@ import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.view.MenuItem;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText et1, et2;
     private TextView tv3;
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "cZg2OXpEhQo6MDisOo0Kf9LcB ";
+    private static final String TWITTER_SECRET = "9KWnJbtNH8gmwxlfMs07O6wb1hmnslweGlI2BHRfSzoB5YgIWz";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_main);
         //new ImageAdapter(this);
         ImageAdapter ia;
@@ -28,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
         gridview.setAdapter(ia);
         Toast toast = Toast.makeText(this,ia.getThumbId(0),Toast.LENGTH_SHORT);
         toast.show();
+
+
 
     }
 }
