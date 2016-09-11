60f639a3fe2b3f97a1e5e65696ed4d61e6ec453e
MendezBruno <brunomendez88@hotmail.com>
2016-09-11 12:16:52 -0300

    piezas infladas en el gridview

--- app/src/main/java/com/example/bruno/ajedrezporcorrespondencia/ImageAdapter.java
+++ app/src/main/java/com/example/bruno/ajedrezporcorrespondencia/ImageAdapter.java
@@ -20,14 +20,6 @@ public class ImageAdapter extends BaseAdapter {
     // Contexto de la aplicación
     private Context mContext;
 
-
-   // Array de identificadores
-      private Integer[] mThumbIds =
-    {
-
-    };
-
-
     public ImageAdapter(Context c) {
         mContext = c;
     }
@@ -41,56 +33,39 @@ public class ImageAdapter extends BaseAdapter {
     }
 
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
-//    public int getThumbId(int position){
-//        return mThumbIds[position];
-//
-//    }
-    public Boolean colorBlanco= true;
-    public View getView(int position, View convertView, ViewGroup parent) {
+
+    public View getView(int position, View view, ViewGroup parent) {
         //ImageView a retornar
 
 
-        if (convertView == null) {
+        if (view == null) {
             /*
             Crear un nuevo Image View de 90x90
             y con recorte alrededor del centro
              */
-//            imageView = new ImageView(mContext);
+//            ImageView  imageView; = new ImageView(mContext);
 //            imageView.setLayoutParams(new GridView.LayoutParams(250,250));
 //            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
 //            imageView.setPadding(0,0,0,0);
 
                 LayoutInflater inflater = (LayoutInflater) mContext
                         .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                imageView =  inflater.inflate(R.layout.grid_item, parent, false);
-            ImageView casillero = (ImageView) imageView.findViewById(R.id.casillero);
-
-        } else {
-            imageView = (ImageView) convertView;
+            view =  inflater.inflate(R.layout.grid_item, parent, false);
         }
-
+        ImageView casillero = (ImageView) view.findViewById(R.id.casillero);
+        ImageView pieza = (ImageView) view.findViewById(R.id.pieza);
 
         //Setear la imagen desde el recurso drawable
-        if ((position % 8 == 0) && position != 0 ) {
-            if (colorBlanco){
-                colorBlanco=false;
+        if (position % 2 == 0) {
+            casillero.setImageResource(R.drawable.casillero1);
             }else{
-            colorBlanco=true;
+            casillero.setImageResource(R.drawable.casillero2);
             }
-        }
 
-        if(colorBlanco){
-                imageView.setImageResource(R.drawable.casillero1);
-            colorBlanco=false;
-        }else
-        {
-            imageView.setImageResource(R.drawable.casillero2);
-            colorBlanco=true;
-        }
-        return imageView;
+        return view;
     }
 
 }
