cf102d97d8d0f13c698d2714ebf7f4745c3e05fb
MendezBruno <brunomendez88@hotmail.com>
2016-10-16 08:46:23 -0300

    Agrego los activity faltantes

---
+++ app/src/main/java/com/example/bruno/ajedrezporcorrespondencia/JuegoNuevo.java
@@ -0,0 +1,13 @@
+package com.example.bruno.ajedrezporcorrespondencia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JuegoNuevo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_juego_nuevo);
+    }
+}
