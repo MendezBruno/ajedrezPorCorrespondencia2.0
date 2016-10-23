a1381e3e1e45a5cb855142f3ae75914f7024712a
MendezBruno <brunomendez88@hotmail.com>
2016-10-23 00:26:57 -0300

    subo navegacion basica

--- app/src/main/java/com/example/bruno/ajedrezporcorrespondencia/MainActivity.java
+++ app/src/main/java/com/example/bruno/ajedrezporcorrespondencia/MainActivity.java
@@ -1,29 +1,40 @@
 package com.example.bruno.ajedrezporcorrespondencia;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainFragmentForMainActivity.Listener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.mainFragment, new MainFragmentForMainActivity(), "Fragment");
+            transaction.commit();
+        }
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
+    @Override
+    public void navigateToGaleria() {
+        Intent intent = new Intent(this, Galeria.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void navigateToChalleger() {
+        Intent intent = new Intent(this, JuegoNuevo.class);
+        startActivity(intent);
+    }
 }
