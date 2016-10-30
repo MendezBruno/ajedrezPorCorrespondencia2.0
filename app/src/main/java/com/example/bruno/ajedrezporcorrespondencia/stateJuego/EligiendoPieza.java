0e0415e9ea7edd82d1ff025ecb69cf80a3eb6432
Ezequiel Ayzenberg <Ezequiel Ayzenberg>
2016-10-30 20:38:07 -0300

    Renombro clase

--- app/src/main/java/com/example/bruno/ajedrezporcorrespondencia/stateJuego/EligiendoPieza.java
+++ app/src/main/java/com/example/bruno/ajedrezporcorrespondencia/stateJuego/EligiendoPieza.java
@@ -4,5 +4,5 @@ package com.example.bruno.ajedrezporcorrespondencia.stateJuego;
  * Created by bruno on 30/10/2016.
  */
 
-public class ElgiendoPieza implements JuegoState {
+public class EligiendoPieza implements JuegoState {
 }
