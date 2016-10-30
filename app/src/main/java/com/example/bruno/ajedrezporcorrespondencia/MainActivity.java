8bd3420545fb38d36ab9e432c568039a6961b789
mariano <marianoguido@yahoo.com.ar>
2016-10-30 17:02:29 -0300

    Agregado login con twitter

--- app/src/main/java/com/example/bruno/ajedrezporcorrespondencia/MainActivity.java
+++ app/src/main/java/com/example/bruno/ajedrezporcorrespondencia/MainActivity.java
@@ -2,20 +2,48 @@ package com.example.bruno.ajedrezporcorrespondencia;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.TwitterAuthProvider;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "cZg2OXpEhQo6MDisOo0Kf9LcB";
+    private static final String TWITTER_SECRET = "9KWnJbtNH8gmwxlfMs07O6wb1hmnslweGlI2BHRfSzoB5YgIWz";
+
+
     Button challegereButton;
     Button galeriaButton;
+    private TwitterLoginButton loginButton;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
+        mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_main);
         challegereButton = (Button) findViewById(R.id.buttonChalleger);
         challegereButton.setOnClickListener(new View.OnClickListener() {
@@ -36,8 +64,47 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        loginButton = (TwitterLoginButton) findViewById(R.id.twitterlogin);
+        loginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                handleTwitterAccessToken(result);
+            }
+            @Override
+            public void failure(TwitterException exception) {
+                Toast.makeText(getApplicationContext(), "Login with Twitter failure", Toast.LENGTH_LONG).show();
+            }
+        });
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Make sure that the loginButton hears the result from any
+        // Activity that it triggered.
+        loginButton.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    private void handleTwitterAccessToken(Result<TwitterSession> result) {
+        AuthCredential credential =  TwitterAuthProvider.getCredential(result.data.getAuthToken().token, result.data.getAuthToken().secret);
+
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener() {
+
+            @Override
+            public void onComplete(@NonNull Task task) {
+
+                if (!task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Authentication failed.",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            });
+
+        }
 
 }
