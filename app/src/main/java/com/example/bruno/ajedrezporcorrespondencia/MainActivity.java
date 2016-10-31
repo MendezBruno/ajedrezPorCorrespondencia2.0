f0bc2c24ca43ce4ad858e08628ede606c02eb1a7
mariano <marianoguido@yahoo.com.ar>
2016-10-31 02:28:13 -0300

    Agregado vista de followers

--- app/src/main/java/com/example/bruno/ajedrezporcorrespondencia/MainActivity.java
+++ app/src/main/java/com/example/bruno/ajedrezporcorrespondencia/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -14,15 +15,24 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.TwitterAuthProvider;
+import com.google.gson.annotations.SerializedName;
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterApiClient;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterAuthToken;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+import com.twitter.sdk.android.core.models.User;
+
+import java.util.List;
 
 import io.fabric.sdk.android.Fabric;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,8 +44,10 @@ public class MainActivity extends AppCompatActivity {
     Button challegereButton;
     Button galeriaButton;
     private TwitterLoginButton loginButton;
+    private Button logOut;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private TwitterSession session;
 
 
     @Override
@@ -69,6 +81,40 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void success(Result<TwitterSession> result) {
                 handleTwitterAccessToken(result);
+                session = Twitter.getSessionManager().getActiveSession();
+                TwitterAuthToken authToken = session.getAuthToken();
+                String token = authToken.token;
+                String secret = authToken.secret;
+                Call<User> call = Twitter.getApiClient(session).getAccountService().verifyCredentials(true, false);
+
+                call.enqueue(new Callback<User>() {
+
+                    @Override
+
+                    public void failure(TwitterException e) {
+                        //If any error occurs handle it here
+                    }
+
+                    public void success(Result<User> userResult) {
+                        //If it succeeds creating a User object from userResult.data
+                        User user = userResult.data;
+
+                        MyTwitterApiClient apiclients=new MyTwitterApiClient(session);
+
+                        apiclients.getCustomService().show(userResult.data.getId(), null, true, true, 100).enqueue( new Callback < Followers > () {
+                            @Override
+                            public void success(Result < Followers > result) {
+                                Log.i("Get success", "" + result.data.users.size());
+                            }
+
+                            @Override
+                            public void failure(TwitterException e) {
+
+                            }
+                        });
+                    }
+                });
+
             }
             @Override
             public void failure(TwitterException exception) {
@@ -76,9 +122,58 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        logOut = (Button) findViewById(R.id.buttonLogOut);
+
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Twitter.getSessionManager().clearActiveSession();
+                Twitter.logOut();
+            }
+        });
+
+
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+
     }
+
+    public class Followers {
+        @SerializedName("users")
+        public final List<User> users;
+
+        public Followers(List<User> users) {
+            this.users = users;
+        }
+    }
+
+    class MyTwitterApiClient extends TwitterApiClient {
+        public MyTwitterApiClient(TwitterSession session) {
+            super(session);
+        }
+
+        public CustomService getCustomService() {
+            return getService(CustomService.class);
+        }
+
+    }
+
+    interface CustomService {@GET("/1.1/followers/list.json")
+    Call<Followers> show(@Query("user_id") Long userId, @Query("screen_name") String var, @Query("skip_status") Boolean var1, @Query("include_user_entities") Boolean var2, @Query("count") Integer var3);
+    }
+
+
+
+
+
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
