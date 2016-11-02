30be93248546ec97177d902b296731c2d4cf7fdb
mariano <marianoguido@yahoo.com.ar>
2016-11-02 12:49:27 -0300

    Realizada interface con twitter

--- app/src/main/java/com/example/bruno/ajedrezporcorrespondencia/MainActivity.java
+++ app/src/main/java/com/example/bruno/ajedrezporcorrespondencia/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -15,24 +14,15 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.TwitterAuthProvider;
-import com.google.gson.annotations.SerializedName;
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
-import com.twitter.sdk.android.core.TwitterApiClient;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
-import com.twitter.sdk.android.core.TwitterAuthToken;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.identity.TwitterLoginButton;
-import com.twitter.sdk.android.core.models.User;
-
-import java.util.List;
 
 import io.fabric.sdk.android.Fabric;
-import retrofit2.Call;
-import retrofit2.http.GET;
-import retrofit2.http.Query;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -82,39 +72,7 @@ public class MainActivity extends AppCompatActivity {
             public void success(Result<TwitterSession> result) {
                 handleTwitterAccessToken(result);
                 session = Twitter.getSessionManager().getActiveSession();
-                TwitterAuthToken authToken = session.getAuthToken();
-                String token = authToken.token;
-                String secret = authToken.secret;
-                Call<User> call = Twitter.getApiClient(session).getAccountService().verifyCredentials(true, false);
-
-                call.enqueue(new Callback<User>() {
-
-                    @Override
-
-                    public void failure(TwitterException e) {
-                        //If any error occurs handle it here
-                    }
-
-                    public void success(Result<User> userResult) {
-                        //If it succeeds creating a User object from userResult.data
-                        User user = userResult.data;
-
-                        MyTwitterApiClient apiclients=new MyTwitterApiClient(session);
-
-                        apiclients.getCustomService().show(userResult.data.getId(), null, true, true, 100).enqueue( new Callback < Followers > () {
-                            @Override
-                            public void success(Result < Followers > result) {
-                                Log.i("Get success", "" + result.data.users.size());
-                            }
-
-                            @Override
-                            public void failure(TwitterException e) {
-
-                            }
-                        });
-                    }
-                });
-
+                new TwitterWrapper(session);
             }
             @Override
             public void failure(TwitterException exception) {
@@ -134,46 +92,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
 
 
     }
-
-    public class Followers {
-        @SerializedName("users")
-        public final List<User> users;
-
-        public Followers(List<User> users) {
-            this.users = users;
-        }
-    }
-
-    class MyTwitterApiClient extends TwitterApiClient {
-        public MyTwitterApiClient(TwitterSession session) {
-            super(session);
-        }
-
-        public CustomService getCustomService() {
-            return getService(CustomService.class);
-        }
-
-    }
-
-    interface CustomService {@GET("/1.1/followers/list.json")
-    Call<Followers> show(@Query("user_id") Long userId, @Query("screen_name") String var, @Query("skip_status") Boolean var1, @Query("include_user_entities") Boolean var2, @Query("count") Integer var3);
-    }
-
-
-
-
-
-
-
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
